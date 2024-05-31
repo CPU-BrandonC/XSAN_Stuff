f95c36a3e7e9873a575432105907ca9280a91074
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-05-31 14:50:39 -0500

    Outlined Script requirements

---
+++ XSAN_Connection_Script.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Check if XSAN disk is already connected
+
+# Check if XSAN Profile is installed
+## Notify if false
+
+# Check if connected to the correct network + XSAN is reachable
+## Notify if either are false
+
+# Check if XSAN service is started
+
+# Attempt to connect QSAN
+## Restart XSAN service if unsuccessful
+
+# Reattempt connection after restarting XSAN
+
+# Notify User
