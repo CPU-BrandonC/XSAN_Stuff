9a9f3206676c2e793c797025e07e4febde377755
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-06-05 16:39:10 -0500

    added some networking logic

--- XSAN_Connection_Script.sh
+++ XSAN_Connection_Script.sh
@@ -13,16 +13,37 @@ then
     echo "ERROR: $xsan_volume already mounted!"
 else
     echo "INFO: $xsan_volume not mounted."
+    exit 1
 fi
 
-
 # Check if XSAN Profile is installed
-## Notify if false
+if sudo profiles -P | grep -q "com.apple.xsan.6"
+then
+    echo "INFO: QSAN profile installed"
+else 
+    echo "ERROR: QSAN profile not installed!"
+    exit 1
+fi
 
 # Check if connected to the correct network + XSAN is reachable
-## Notify if either are false
-network_interface_name=$(route -n get $xsan_volume | grep "interface" | awk -F ':\ ' '{print $2}')
+if ping -c 1 $xsan_volume
+then
+    echo "INFO: $xsan_volume is reachable."
+    # Checks which interface is used to connect to QSAN
+    network_interface_name=$(route -n get $xsan_volume | grep "interface" | awk -F ':\ ' '{print $2}')
+    echo "INFO: Network interface is $network_interface_name."
+else
+    echo "ERROR. Unable to ping QSAN"
+    exit 1
+fi
+
+# Checks interface speed
 network_interface_speed=$(networksetup -getmedia $network_interface_name | grep "Active" | awk -F ': ' '{print $2}')
+if [[ if $network_interface_speed = "" ]]
+
+
+
+
 
 # Check if XSAN service is started
 
