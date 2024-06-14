75b280202c45d85fd93b5f26db499df2f605b6b4
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-06-14 17:21:35 -0500

    Added a few more bells and whistles. Accomplished nothing

--- XSAN_Connection_Script.sh
+++ XSAN_Connection_Script.sh
@@ -1,28 +1,75 @@
 #!/bin/zsh
 
+# Set the notification level. Must have SwiftDialog installed.
+notifications="debug"
+# debug = all messages
+# error = errors only
+# error_success = errors and success
+# success = success only
+
 xsan_volume="XSAN_Volume" # XSAN volume name
 
-# Connect to QSAN
+# Don't change anything below this line
+notify_failed="false"
+
+notify_user() {
+if [ -f "/usr/local/bin/dialog" ]
+then
+    echo "$1"
+    /usr/local/bin/dialog --notification --title "$xsan_volume" --message "$1"
+else
+    if [[ "$notify_failed" = "false" ]]
+    then
+        echo "WARNING: Swift Dialog not installed. Proceeding..."
+        notify_failed="true"
+        echo "$1"
+    else
+        echo "$1"
+    fi
+fi
+}
+
 connect_qsan() { 
-    echo "INFO: Connecting to QSAN"
+    echo "INFO: Connecting to $xsan_volume..."
     xsanctl mount $xsan_volume
+    if [ $? = 0 ]
+    then
+        echo "SUCCESS: Successfully mounted $xsan_volume."
+        exit 0
+    else
+        echo "ERROR: Failed to mount $xsan_volume."
+        exit 1
+    fi
 }
 
 restart_xsan_service() {
-    if launchctl list | grep -q "xsan"
+    if launchctl list | grep -q "xsan" # check if XSAN is already running
     then
         echo "INFO: XSAN service is running. Restarting service."
         echo "INFO: Unloading..."
-        launchctl unload /System/Library/LaunchDaemons/com.apple.xsan.plist | true
+        launchctl unload /System/Library/LaunchDaemons/com.apple.xsan.plist | true # this sometimes fails so return true even if it fails
         echo "INFO: Loading..."
         launchctl load -w /System/Library/LaunchDaemons/com.apple.xsan.plist
         if [ $? = 0 ]
         then
             echo "INFO: Successfully restarted XSAN service."
-            sleep 8 # wait for xsan service to attept to connect to QSAN
-            
+            sleep 8 # XSAN will automatically attempt to mount volumes after starting. This delay prevents the script from also attempting to mount it at the same time 
+        else
+            echo "ERROR: Failed to restart XSAN service"
+            exit 1
+        fi
     else
-
+        echo "INFO: Loading XSAN service"
+        launchctl load -w /System/Library/LaunchDaemons/com.apple.xsan.plist
+        if [ $? = 0 ]
+        then
+            echo "INFO: Successfully restarted XSAN service."
+            sleep 8 # XSAN will automatically attempt to mount volumes after starting. This delay prevents the script from also attempting to mount it at the same time
+        else
+            echo "ERROR: Failed to restart XSAN service"
+            exit 1
+        fi
+    fi
 }
 
 echo "------------------------------------"
@@ -34,9 +81,9 @@ echo "INFO: Checking if $xsan_volume is already mounted..."
 if mount | grep -q "/Volumes/$xsan_volume"
 then
     echo "ERROR: $xsan_volume already mounted!"
+    exit 1
 else
     echo "INFO: $xsan_volume not mounted."
-    exit 1
 fi
 
 # Check if XSAN Profile is installed
@@ -77,9 +124,3 @@ then
 else
     echo "WARNING: XSAN service not running"
 fi
-
-
-
-# Reattempt connection after restarting XSAN
-
-# Notify User
