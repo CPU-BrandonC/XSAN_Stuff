6ac9f0187c6de2c2df1dcea911b949200917b900
CPU-BrandonC <brandon.chesser@c-p-u.com>
2024-06-17 13:41:49 -0500

    Added user notifications

--- XSAN_Connection_Script.sh
+++ XSAN_Connection_Script.sh
@@ -1,43 +1,45 @@
 #!/bin/zsh
 
-# Set the notification level. Must have SwiftDialog installed.
-notifications="debug"
-# debug = all messages
-# error = errors only
-# error_success = errors and success
-# success = success only
+# Enable Notifications. Must have SwiftDialog installed.
+notifications_enabled="true"
 
-xsan_volume="XSAN_Volume" # XSAN volume name
+# XSAN volume name
+xsan_volume="XSAN_Volume"
 
 # Don't change anything below this line
 notify_failed="false"
 
 notify_user() {
-if [ -f "/usr/local/bin/dialog" ]
-then
-    echo "$1"
-    /usr/local/bin/dialog --notification --title "$xsan_volume" --message "$1"
-else
-    if [[ "$notify_failed" = "false" ]]
+    if [ -f "/usr/local/bin/dialog" ]
     then
-        echo "WARNING: Swift Dialog not installed. Proceeding..."
-        notify_failed="true"
-        echo "$1"
+        if [ "$notifications_enabled" = "true" ]
+     then
+            echo "$1"
+            /usr/local/bin/dialog --notification --title "$xsan_volume" --message "$1"
+        else
+            echo "$1"
+        fi
     else
-        echo "$1"
+        if [[ "$notify_failed" = "false" ]]
+        then
+            echo "WARNING: Swift Dialog not installed. Proceeding..."
+            notify_failed="true"
+            echo "$1"
+        else
+            echo "$1"
+        fi
     fi
-fi
 }
 
-connect_qsan() { 
+connect_xsan() { 
     echo "INFO: Connecting to $xsan_volume..."
     xsanctl mount $xsan_volume
     if [ $? = 0 ]
     then
-        echo "SUCCESS: Successfully mounted $xsan_volume."
+        notify_user "SUCCESS: Successfully mounted $xsan_volume."
         exit 0
     else
-        echo "ERROR: Failed to mount $xsan_volume."
+        notify_user "ERROR: Failed to mount $xsan_volume."
         exit 1
     fi
 }
@@ -54,8 +56,9 @@ restart_xsan_service() {
         then
             echo "INFO: Successfully restarted XSAN service."
             sleep 8 # XSAN will automatically attempt to mount volumes after starting. This delay prevents the script from also attempting to mount it at the same time 
+            connect_xsan
         else
-            echo "ERROR: Failed to restart XSAN service"
+            notify_user "ERROR: Failed to restart XSAN service"
             exit 1
         fi
     else
@@ -65,22 +68,23 @@ restart_xsan_service() {
         then
             echo "INFO: Successfully restarted XSAN service."
             sleep 8 # XSAN will automatically attempt to mount volumes after starting. This delay prevents the script from also attempting to mount it at the same time
+            connect_xsan
         else
-            echo "ERROR: Failed to restart XSAN service"
+            notify_user "ERROR: Failed to restart XSAN service"
             exit 1
         fi
     fi
 }
 
 echo "------------------------------------"
-echo "Starting XSAN connection utility"
+notify_user "Starting XSAN connection utility"
 echo "------------------------------------"
 
 # Check if XSAN disk is already connected
 echo "INFO: Checking if $xsan_volume is already mounted..."
 if mount | grep -q "/Volumes/$xsan_volume"
 then
-    echo "ERROR: $xsan_volume already mounted!"
+    notify_user "ERROR: $xsan_volume already mounted!"
     exit 1
 else
     echo "INFO: $xsan_volume not mounted."
@@ -89,9 +93,9 @@ fi
 # Check if XSAN Profile is installed
 if sudo profiles -P | grep -q "com.apple.xsan."
 then
-    echo "INFO: QSAN profile installed"
+    echo "INFO: $xsan_volume profile installed"
 else 
-    echo "ERROR: QSAN profile not installed!"
+    notify_user "ERROR: $xsan_volume profile not installed!"
     exit 1
 fi
 
@@ -99,11 +103,11 @@ fi
 if ping -c 1 $xsan_volume
 then
     echo "INFO: $xsan_volume is reachable."
-    # Checks which interface is used to connect to QSAN
+    # Checks which interface is used to connect to the XSAN Volume
     network_interface_name=$(route -n get $xsan_volume | grep "interface" | awk -F ':\ ' '{print $2}')
     echo "INFO: Network interface is $network_interface_name."
 else
-    echo "ERROR. Unable to ping QSAN!"
+    notify_user "ERROR. Unable to ping $xsan_volume!"
     exit 1
 fi
 
@@ -117,10 +121,6 @@ else
     echo "WARNING: Network interface speed is $network_interface_speed. Connect to 10G network and confirm network service order. Continuing..."
 fi
 
-# Check if XSAN service is started
-if launchctl list | grep -q "xsan"
-then
-    echo "INFO: XSAN service is currently running"
-else
-    echo "WARNING: XSAN service not running"
-fi
+restart_xsan_service
+
+connect_xsan
